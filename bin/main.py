513c7cb310a6710b00ccd9bcf2fd5a018e5fc249
Liu Tianhua <648930239@qq.com>
2026-09-08 14:45:40 +0800

    [fix] fix incorrect string concatenation
    root cause: missing a '+' between timestap and log_tag
    solution: add a '+'
    
    Signed-off-by: Liu Tianhua <648930239@qq.com>

--- bin/main.py
+++ bin/main.py
@@ -222,7 +222,7 @@ def run_task(task_flag: str, out_dir: Path, log_tag: 'Optional[str]') -> bool:
     builder = CommandBuilder()
 
     timestamp = time.strftime("%Y%m%d_%H_%M")
-    log_dir = out_dir / f"{config.fileName}" / task_name / (timestamp + (log_tag if log_tag is not None else ''))
+    log_dir = out_dir / f"{config.fileName}" / task_name / (timestamp + ('+'+log_tag if log_tag is not None else ''))
     log_dir.mkdir(parents=True, exist_ok=True)
 
     if task_name == "TritonDump":
